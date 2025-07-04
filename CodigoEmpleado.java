e198dadc87262868397dc6b86c464ebbae2570a5
chahla1 <cbouchot@myuax.com>
2025-07-04 19:17:22 +0200

    commit

--- CodigoEmpleado.java
+++ CodigoEmpleado.java
@@ -1,34 +1,48 @@
-//nos piden crear una funcion hash para almacenar tabajadores en una empresa en donde las sedes donde trabajan y los numeros de tres digitos se encuentran en la clase CodigoEmpleado,
-//los datos se tienen que almacenar en una tabla llamada tbl
-public class CodigoEmpleado {
-    /*A: españa
-    b: francia
-    c: alemania
-    d: reino unido
-    e: paises pbajos
-     */
+
     /* hacer una funcion hash con codigos de trabajador: (A,23), (C,134) y (E, 1)*/
+    import java.util.HashMap;
+import java.util.Map;
 
-    public CodigoEmpleado getCodigo() {
-        return codigo;
+    public class CodigoEmpleado {
+        private String codPais; // codigo del pais
+        private int codEmpleado; // numero del empleado
+
+        public CodigoEmpleado(String codPais, int codEmpleado) {
+            this.codPais = codPais;
+            this.codEmpleado = codEmpleado;
+        }
+
+        public String getCodPais() {
+            return codPais;
+        }
+
+        public int getCodEmpleado() {
+            return codEmpleado;
+        }
+
+        public int getHash() {
+            int codPaisValue = codPais.charAt(0) - 'A'; // convertimos el codigo del pais a un numero
+            return codPaisValue * 1000 + codEmpleado; // ahora generamos el Hash
+        }
+
+        public static void main(String[] args) {
+            // Creamos una tabla para almacenar los empleados usando HashMap
+            Map<Integer, CodigoEmpleado> tbl = new HashMap<>();
+
+            // Utilizamos los ejemplos de los empleados
+            CodigoEmpleado emp1 = new CodigoEmpleado("A", 23);
+            CodigoEmpleado emp2 = new CodigoEmpleado("C", 134);
+            CodigoEmpleado emp3 = new CodigoEmpleado("E", 1);
+
+            // almacenamos los empleados en las tablas usando Hash
+            tbl.put(emp1.getHash(), emp1);
+            tbl.put(emp2.getHash(), emp2);
+            tbl.put(emp3.getHash(), emp3);
+
+            // Ahora imprimos las tablas de hash
+            for (Map.Entry<Integer, CodigoEmpleado> entry : tbl.entrySet()) {
+                System.out.println("Hash: " + entry.getKey() + ", Empleado: " +
+                        entry.getValue().getCodPais() + ", " + entry.getValue().getCodEmpleado());
+            }
+        }
     }
-
-    public String getCodPais() {
-        return null;
-    }
-
-    public int getCodEmpleado() {
-        return 0;
-    }
-
-
-    CodigoEmpleado codigo;
-
-    public int getHash() {
-        int codPais = codigo.getCodPais().charAt(0) - 'A';
-        int hash = codPais * 1000 + codigo.getCodEmpleado();
-        return hash;
-        int codEmpleado = codigo.getCodEmpleado().charAt(0) - 'A';
-    }
-
-}
